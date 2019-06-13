89d05f1669fa38f58c6beed826707bacf16de245
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-06-13 23:43:39 +0900

    Initial Commit

---
+++ write-ruby-characters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  write-ruby-characters
+//
+//  Created by junpei ono on 2019/06/13.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ write-ruby-charactersTests/write_ruby_charactersTests.swift
@@ -0,0 +1,34 @@
+//
+//  write_ruby_charactersTests.swift
+//  write-ruby-charactersTests
+//
+//  Created by junpei ono on 2019/06/13.
+//  Copyright © 2019 com.swiswiswift. All rights reserved.
+//
+
+import XCTest
+@testable import write_ruby_characters
+
+class write_ruby_charactersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
